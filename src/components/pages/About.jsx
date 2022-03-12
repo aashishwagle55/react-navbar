f4e926ac9d95c456f81c223b6a102d870036ff25
Aashish Wagle <aashishwagle55@gmail.com>
2022-03-12 17:24:05 -0500

    created About component

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>Everything Twice</h1>
+      <p>Everything TWICE Related</p>
+    </Fragment>
+  );
+};
+
+export default About;
